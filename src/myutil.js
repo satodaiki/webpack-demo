08973598721012eed810f96bfb392534780194b8
satodai <banana5388@gmail.com>
2019-10-10 12:54:39 +0900

    feature: chapter2の実装

---
+++ src/myutil.js
@@ -0,0 +1,13 @@
+let message = 'hello webpack';
+
+export const APP_NAME = '速習Webpack';
+
+export function hello() {
+  console.log(message);
+}
+
+export class Figure {
+  static getTriangle(base, height) {
+    return base * height / 2;
+  }
+}
